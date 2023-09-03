3dfdc0e86fbab752d8b6c7301d68719740bca02b
Jeroen Ouweneel <jeroen@bytewriters.com>
2023-09-03 02:24:53 +0000

    Move jest global setup/teardown to shared

---
+++ test/setup.ts
@@ -0,0 +1,29 @@
+import { config as loadEnvFile } from 'dotenv';
+import { resolve } from 'path';
+
+const projectRoot = resolve(__dirname, '../..');
+const getEnvFile = (rel_path: string) => `${projectRoot}/${rel_path}/.env`
+
+const envFiles = [
+  '.',
+  'docker/influxdb',
+  'docker/postgres',
+  'docker/redis',
+  'docker/server'
+].map(getEnvFile);
+
+const overrides = {
+  DB_HOST: 'localhost',
+  INFLUX_HOST: 'localhost',
+  REDIS_HOST: 'localhost',
+}
+
+export default async () => {
+  if (process.env.POSTGRES_PASSWORD) return;
+
+  for (const path of envFiles) loadEnvFile({ path });
+
+  for (const [ key, value ] of Object.entries(overrides)) {
+    process.env[key] = value;
+  }
+}
